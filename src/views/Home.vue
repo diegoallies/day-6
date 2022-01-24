8014239719a56876c61c6804a55722f351fa0ae2
Encrypto27 <diegoallies27@gmail.com>
2022-01-24 09:28:33 +0200

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis similique quis culpa commodi tenetur, sunt ad consequuntur asperiores iusto repudiandae maiores obcaecati alias, natus repellendus delectus aperiam expedita ex, eos vitae nulla atque. Consequuntur dolore minima maiores officiis nisi amet incidunt quod porro officia qui! Id et ipsa minus animi ipsum odio debitis dicta aut dolorem, ea cupiditate natus adipisci, sed asperiores. Voluptas, ea facilis perspiciatis deserunt nulla ratione necessitatibus praesentium beatae hic eligendi quibusdam commodi alias vitae corrupti vero incidunt fugiat. Provident qui quam harum expedita! Quisquam veniam officiis dolore distinctio nulla inventore nesciunt incidunt numquam laudantium recusandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis similique quis culpa commodi tenetur, sunt ad consequuntur asperiores iusto repudiandae maiores obcaecati alias, natus repellendus delectus aperiam expedita ex, eos vitae nulla atque. Consequuntur dolore minima maiores officiis nisi amet incidunt quod porro officia qui! Id et ipsa minus animi ipsum odio debitis dicta aut dolorem, ea cupiditate natus adipisci, sed asperiores. Voluptas, ea facilis perspiciatis deserunt nulla ratione necessitatibus praesentium beatae hic eligendi quibusdam commodi alias vitae corrupti vero incidunt fugiat. Provident qui quam harum expedita! Quisquam veniam officiis dolore distinctio nulla inventore nesciunt incidunt numquam laudantium recusandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis similique quis culpa commodi tenetur, sunt ad consequuntur asperiores iusto repudiandae maiores obcaecati alias, natus repellendus delectus aperiam expedita ex, eos vitae nulla atque. Consequuntur dolore minima maiores officiis nisi amet incidunt quod porro officia qui! Id et ipsa minus animi ipsum odio debitis dicta aut dolorem, ea cupiditate natus adipisci, sed asperiores. Voluptas, ea facilis perspiciatis deserunt nulla ratione necessitatibus praesentium beatae hic eligendi quibusdam commodi alias vitae corrupti vero incidunt fugiat. Provident qui quam harum expedita! Quisquam veniam officiis dolore distinctio nulla inventore nesciunt incidunt numquam laudantium recusandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure facilis similique quis culpa commodi tenetur, sunt ad consequuntur asperiores iusto repudiandae maiores obcaecati alias, natus repellendus delectus aperiam expedita ex, eos vitae nulla atque. Consequuntur dolore minima maiores officiis nisi amet incidunt quod porro officia qui! Id et ipsa minus animi ipsum odio debitis dicta aut dolorem, ea cupiditate natus adipisci, sed asperiores. Voluptas, ea facilis perspiciatis deserunt nulla ratione necessitatibus praesentium beatae hic eligendi quibusdam commodi alias vitae corrupti vero incidunt fugiat. Provident qui quam harum expedita! Quisquam veniam officiis dolore distinctio nulla inventore nesciunt incidunt numquam laudantium recusandae.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
